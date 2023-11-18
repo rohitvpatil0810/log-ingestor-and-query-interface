eda4a16c30aefdeee06127a2902beaaaddb8f4aa
Rohit Patil <rohitvpatil0810@gmail.com>
2023-11-18 12:00:51 +0530

    🚀 Implemented route for fetching all logs from the database

--- backend/controllers/log.controllers.js
+++ backend/controllers/log.controllers.js
@@ -15,6 +15,17 @@ const ingestLogs = async (req, res) => {
   }
 };
 
+const getAllLogs = async (req, res) => {
+  try {
+    const logs = await Log.find();
+    res.status(200).json({ success: true, data: logs });
+  } catch (error) {
+    console.error("Error fetching logs:", error);
+    res.status(500).json({ success: false, error: "Internal Server Error" });
+  }
+};
+
 module.exports = {
   ingestLogs,
+  getAllLogs,
 };
--- backend/routes/log.router.js
+++ backend/routes/log.router.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const logController = require("../controllers/log.controllers");
 const logRouter = Router();
 
+logRouter.get("/", logController.getAllLogs);
 logRouter.post("/", logController.ingestLogs);
 
 module.exports = logRouter;
